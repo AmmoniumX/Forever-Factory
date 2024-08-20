3ad865ab89add72b6bd61321239af5eae22f56b0
AmmoniumX <diego.garcia.cr@gmail.com>
2024-08-20 11:19:33 +0100

    Added oil sand recipes for all oil variants. Dear Kubejs: proper fluid tag support when?

--- overrides/kubejs/server_scripts/Create/createCrusher.js
+++ overrides/kubejs/server_scripts/Create/createCrusher.js
@@ -3,7 +3,7 @@ console.info('[AMMONIUM@KUBEJS]: createCrusher.js loaded')
 
 ServerEvents.recipes(e => {
     e.recipes.create.crushing([Item.of('minecraft:sand', 2), Item.of('immersiveengineering:dust_saltpeter').withChance(0.125)], '#forge:sandstone/colorless')
-    e.recipes.create.crushing(['#forge:dusts/rutile', Item.of('#forge:dusts/rutile').withChance(0.25)], '#forge:raw_materials/rutile')
+    e.recipes.create.crushing([Item.of('#forge:dusts/rutile'), Item.of('#forge:dusts/rutile').withChance(0.25)], '#forge:raw_materials/rutile')
     e.recipes.create.milling(['ae2:certus_quartz_crystal', Item.of('ae2:certus_quartz_crystal').withChance(0.5), Item.of('ae2:certus_quartz_crystal', 2).withChance(0.25)], 'kubejs:certus_seed')
     e.recipes.create.crushing(['ae2:sky_dust', Item.of('ae2:sky_dust').withChance(0.25)], 'ae2:sky_stone_block')
     e.recipes.create.milling(['ae2:sky_dust', Item.of('minecraft:redstone', 2).withChance(0.5), Item.of("ae2:certus_quartz_dust", 2).withChance(0.5), Item.of("kubejs:integrated_circuit", 3).withChance(0.5)], 'kubejs:computing_core_burnt')
