3bd2e58c91d2113d71a60361b69b56785e6b021a
Diego Garcia <diego.garcia.cr@gmail.com>
2024-08-22 21:39:34 +0100

    Fix: set bottle emptying recipes to require strong NBT

--- overrides/kubejs/server_scripts/unified/emptying.js
+++ overrides/kubejs/server_scripts/unified/emptying.js
@@ -8,7 +8,7 @@ ServerEvents.recipes(e => {
     e.recipes.thermal.crucible(Fluid.of('kubejs:gelid_cryotheum', 250), 'kubejs:cryotheum_crystal').energy(1200)
     e.recipes.thermal.crucible(Fluid.of('kubejs:blazing_pyrotheum', 250), 'powah:crystal_blazing').energy(1200)
 
-    e.recipes.create.emptying([Fluid.of('kubejs:liquid_sunlight', 250), 'naturesaura:bottle_two_the_rebottling'], Item.of('naturesaura:aura_bottle', '{stored_type:"naturesaura:overworld"}'))
-    e.recipes.create.emptying([Fluid.of('kubejs:liquid_ghosts', 250), 'naturesaura:bottle_two_the_rebottling'], Item.of('naturesaura:aura_bottle', '{stored_type:"naturesaura:nether"}'))
-    e.recipes.create.emptying([Fluid.of('kubejs:liquid_darkness', 250), 'naturesaura:bottle_two_the_rebottling'], Item.of('naturesaura:aura_bottle', '{stored_type:"naturesaura:end"}'))
+    e.recipes.create.emptying([Fluid.of('kubejs:liquid_sunlight', 250), 'naturesaura:bottle_two_the_rebottling'], Item.of('naturesaura:aura_bottle', '{stored_type:"naturesaura:overworld"}').strongNBT())
+    e.recipes.create.emptying([Fluid.of('kubejs:liquid_ghosts', 250), 'naturesaura:bottle_two_the_rebottling'], Item.of('naturesaura:aura_bottle', '{stored_type:"naturesaura:nether"}').strongNBT())
+    e.recipes.create.emptying([Fluid.of('kubejs:liquid_darkness', 250), 'naturesaura:bottle_two_the_rebottling'], Item.of('naturesaura:aura_bottle', '{stored_type:"naturesaura:end"}').strongNBT())
 })
